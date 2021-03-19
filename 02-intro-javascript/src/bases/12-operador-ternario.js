413ddbabfeab88c11887fb5fae7fa8c6bd774f5c
Richie Cedillo <rcedilloal@gmail.com>
2021-03-18 19:29:15 -0600

    [add] Bases JS - operador ternario

---
+++ 02-intro-javascript/src/bases/12-operador-ternario.js
@@ -0,0 +1,15 @@
+const activo = true;
+
+//let mensaje = '';
+
+// if (activo) {
+//   mensaje = 'activo';
+// } else {
+//   mensaje = 'inactivo';
+// }
+
+//const mensaje = (!activo) ? 'activo' : 'inactivo';
+//const mensaje = (!activo) ? 'activo' : null;
+const mensaje = !activo && 'activo'
+
+console.log('mensaje: ', mensaje);
