410fad71753b36adedf03292ea94f45fa44d9717
Richie Cedillo <rcedilloal@gmail.com>
2021-04-05 19:52:25 -0500

    [add]Se crea el docs para deploy

---
+++ 04-gif-expert-app/docs/precache-manifest.a2d81597526eed8f7021f1ac41210faf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8636e9e2c005fa29359b8b8d5a4f760",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bb0c0d63d14c13ff3f2",
+    "url": "/static/css/main.b2e1e9dd.chunk.css"
+  },
+  {
+    "revision": "e13293c431772057daf3",
+    "url": "/static/js/2.7fae8f10.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.7fae8f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bb0c0d63d14c13ff3f2",
+    "url": "/static/js/main.ae0b123d.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
