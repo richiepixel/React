d93061af7f0846b5f879c9f5d067d0b134aaf2b5
Richie Cedillo <rcedilloal@gmail.com>
2021-04-15 19:57:51 -0500

    [add] Se agrega el componente SimpleForm para el uso del hook useEffect

---
+++ 05-hook-app/src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react';
+import './css/effects.css';
+
+const SimpleForm = () => {
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+  });
+
+  const { name, email } = formState;
+
+  useEffect(() => {
+    console.log('hey!');
+  }, []);
+
+  useEffect(() => {
+    console.log('formState cambió');
+  }, [formState]);
+
+  useEffect(() => {
+    console.log('email cambió');
+  }, [email]);
+
+  const handleInputChange = ({ target }) => {
+    console.log(target.value);
+    setFormState({
+      ...formState,
+      [target.name]: target.value,
+    });
+  };
+
+  return (
+    <>
+      <h1>useEffect</h1>
+      <hr />
+      <div className="form-group">
+        <input
+          type="text"
+          name="name"
+          className="form-control"
+          placeholder="Tu nombre"
+          autoComplete="off"
+          value={name}
+          onChange={handleInputChange}
+        />
+      </div>
+      <br />
+      <div className="form-group">
+        <input
+          type="text"
+          name="email"
+          className="form-control"
+          placeholder="Tu correo electrónico"
+          autoComplete="off"
+          value={email}
+          onChange={handleInputChange}
+        />
+      </div>
+    </>
+  );
+};
+
+export default SimpleForm;
