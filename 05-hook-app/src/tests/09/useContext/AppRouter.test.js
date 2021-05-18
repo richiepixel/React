516715dda040d96deb9d1718a6e9a4409a16ce27
Richie Cedillo <rcedilloal@gmail.com>
2021-05-17 20:22:02 -0500

    [add] Prueba inicial en el componente <AppRouter/>

---
+++ 05-hook-app/src/tests/09/useContext/AppRouter.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { AppRouter } from '../../../components/09-useContext/AppRouter';
+import { UserContext } from '../../../components/09-useContext/UserContext';
+
+describe('Pruebas en <AppRouter/>', () => {
+  const user = {
+    id: 7,
+    name: 'Richie',
+  };
+
+  const wrapper = mount(
+    <UserContext.Provider value={{ user }}>
+      <AppRouter />
+    </UserContext.Provider>
+  );
+
+  test('debe de mostrarse correctamente', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
