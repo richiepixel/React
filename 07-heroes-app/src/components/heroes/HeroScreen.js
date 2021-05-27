312c2b7e5d675e14485e0aa4e62356f42d1fa61e
Richie Cedillo <rcedilloal@gmail.com>
2021-05-27 04:39:56 -0500

    [add] Se agregan los componentes HeroScreen, HeroesList y DashboardRoutes, se utiliza useMemo

--- 07-heroes-app/src/components/heroes/HeroScreen.js
+++ 07-heroes-app/src/components/heroes/HeroScreen.js
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { useParams, Redirect } from 'react-router-dom';
+import { getHeroById } from '../../selectors/getHeroById';
 
-export const HeroScreen = () => {
+export const HeroScreen = ({ history }) => {
+  const { heroId } = useParams();
+  const hero = useMemo(() => getHeroById(heroId), [heroId]);
+
+  if (!hero) {
+    return <Redirect to="/" />;
+  }
+
+  const handleReturn = () => {
+    if (history.length <= 2) {
+      history.push('/');
+    } else {
+      history.goBack();
+    }
+  };
+
+  const {
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearance,
+    characters,
+  } = hero;
+
+  console.log(hero);
   return (
-    <div>
-      <h1>HeroScreen</h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          src={`../assets/heroes/${heroId}.jpg`}
+          alt={superhero}
+          className="img-thumbnail"
+        />
+      </div>
+      <div className="col-8">
+        <h3>{superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Alter ego: </b> {alter_ego}
+          </li>
+          <li className="list-group-item">
+            <b>Publisher: </b> {publisher}
+          </li>
+          <li className="list-group-item">
+            <b>First apperance: </b> {first_appearance}
+          </li>
+        </ul>
+        <hr />
+        <h5>Characters</h5>
+        <p>{characters}</p>
+        <button className="btn btn-outline-info" onClick={handleReturn}>
+          Return
+        </button>
+      </div>
     </div>
   );
 };
