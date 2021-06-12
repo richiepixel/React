4e6fef8447e3bcecfc6ffeb57bdda379ef568d2a
Richie Cedillo <rcedilloal@gmail.com>
2021-06-12 15:24:35 -0500

    [add] JournalApp init y routes

---
+++ 08-journal-app/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles/styles.scss';
+import { JournalApp } from './JournalApp';
+
+ReactDOM.render(<JournalApp />, document.getElementById('root'));
---
+++ 08-journal-app/src/routers/AuthRouter.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import { LoginScreen } from '../components/auth/LoginScreen';
+import { RegisterScreen } from '../components/auth/RegisterScreen';
+
+export const AuthRouter = () => {
+  return (
+    <div>
+      <Switch>
+        <Route path="/auth/login" component={LoginScreen} />
+        <Route path="/auth/register" component={RegisterScreen} />
+
+        <Redirect to="/auth/login" />
+      </Switch>
+    </div>
+  );
+};
