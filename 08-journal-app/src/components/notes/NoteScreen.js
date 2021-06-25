3a9b66284763daf74aa42b5a4185ba14ef3e15e8
Richie Cedillo <rcedilloal@gmail.com>
2021-06-25 18:41:02 -0500

    [chg] useForm para no ciclar el useEffect al renderizar la nota. [add] useRef para la nota actual y actualización de la nota activa en el store

--- 08-journal-app/src/components/notes/NoteScreen.js
+++ 08-journal-app/src/components/notes/NoteScreen.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { activeNote } from '../../actions/notes';
 import { useForm } from '../../hooks/useForm';
 import { NotesAppBar } from './NotesAppBar';
 
 export const NoteScreen = () => {
-  const { active: note } = useSelector((state) => state.notes);
-  const [formValues, handleInputChange] = useForm(note);
+  const dispatch = useDispatch();
 
+  const { active: note } = useSelector((state) => state.notes);
+  const [formValues, handleInputChange, reset] = useForm(note);
   const { body, title, url } = formValues;
+
+  const activeId = useRef(note.id);
+
+  useEffect(() => {
+    if (note.id !== activeId.current) {
+      reset(note);
+      activeId.current = note.id;
+    }
+  }, [note, reset]);
+
+  useEffect(() => {
+    dispatch(activeNote(formValues.id, { ...formValues }));
+  }, [formValues, dispatch]);
 
   return (
     <div className="notes__main-content">
@@ -18,12 +33,14 @@ export const NoteScreen = () => {
           type="text"
           placeholder="Some awesome title"
           autoComplete="off"
+          name="title"
           value={title}
           onChange={handleInputChange}
         />
         <textarea
           className="notes__textarea"
           placeholder="How do you feel today?"
+          name="body"
           value={body}
           onChange={handleInputChange}
         ></textarea>
